64d8a4eaccd842e5e89d1a61cd03d8db4cb26697
Achilike Justice <achilikejustice21@gmail.com>
2025-07-06 07:42:37 +0100

    Add files via upload

---
+++ headphones/8-script.js
@@ -0,0 +1,9 @@
+"use strict"
+
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
+
+
+hamburger.addEventListener("click", ()=>{
+    navLinks.classList.toggle("flex")
+})
